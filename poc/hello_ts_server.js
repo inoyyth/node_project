7638418620a057a910533cbf053121d02871e284
Inoy Yth <supri170845@gmail.com>
2017-12-13 12:58:19 +0700

    poc rea

--- poc/hello_ts_server.js
+++ poc/hello_ts_server.js
@@ -1,14 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var http = require('http');
-http.createServer(function (request, response) {
+const http = require('http');
+http.createServer((request, response) => {
     //create connection
-    var client = yield mongodb.connect('mongodb://localhost:27017/nodejs_demo');
+    let client = yield mongodb.connect('mongodb://localhost:27017/nodejs_demo');
     console.log(client);
     //insert data
-    var status_code = 200;
-    var body_response = 'Hello, this is REA type script server';
-    var demo_response = ["data 1", "data 2"];
+    let result = yield client.collection('test').insert({ name: "ihdi" });
+    //get
+    let name = yield client.collection('test').find({}).toArray();
+    let status_code = 200;
+    let body_response = 'Hello, this is REA type script server';
+    let demo_response = ["data 1", "data 2"];
     response.writeHead(status_code);
     response.write(body_response);
     response.end();
