240d5a694582b67750da3f4a24818b50146f9a1b
inoyyth <supri170845@gmail.com>
2018-01-03 07:05:06 +0700

    nodeauth update student

--- nodeauth/routes/students.js
+++ nodeauth/routes/students.js
@@ -1,9 +1,11 @@
 var express = require("express");
- var router = express.Router();
-
-/* GET home page. */
+var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+var student = require('../models/student');
+/* GET student index page. */
 router.get('/', ensureAuthenticated , function (req, res, next) {
-    res.render('index', { title: 'Members' });
+    res.render('student', { title: 'Students' });
 });
 
 function ensureAuthenticated(req ,res, next) {
