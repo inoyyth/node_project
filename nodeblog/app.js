6b943a0cbe92bf0dc2a2c3a42f0178cf85516d34
inoyyth <supri170845@gmail.com>
2018-01-28 18:11:46 +0700

    update nodeblog fix index page

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -20,6 +20,12 @@ var categories = require('./routes/categories');
 
 var app = express();
 
+app.locals.moment = moment;
+app.locals.truncateText = function(text, length) {
+  var truncatedText = text.substring(0, length);
+  return truncatedText;
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
